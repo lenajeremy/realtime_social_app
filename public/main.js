0780bd7b455b1e7ab615692672e928f5c12d0e08
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-09-17 16:22:13 +0100

    done with javascript...i believe

--- public/main.js
+++ public/main.js
@@ -1,16 +1,28 @@
-const socket = io();
+document.addEventListener('DOMContentLoaded', () => {
+  const username = prompt('what is your name??')
 
-function addNewMessage(message){
-  document.querySelector('.messageContainer').innerHTML+= `<div class='message'>${message}</div>`
-}
-
-socket.on('message', message => addNewMessage(message))
-const $form = document.querySelector('form');
-
-
-
-$form.addEventListener('submit', e => {
-  e.preventDefault();
-  let message = e.target.elements[0].value;
-  socket.emit('msgFromFront', message)
+  const socket = io();
+  
+  
+  function addNewMessage(message){
+    console.log(message.content);
+    document.querySelector('.messageContainer').innerHTML+= `<div class='message ${message.sender === username ? 'user' : ''}'>${message.content}</div>`
+    document.querySelector('.messageContainer').lastElementChild.scrollIntoView()
+    document.body.style.background = 'white';
+  }
+  
+  socket.on('message', message => addNewMessage(message));
+  socket.on('showtyping', stuff => document.body.style.background ='blue');
+  const $form = document.querySelector('form');
+  
+  $form.addEventListener('submit', e => {
+    e.preventDefault();
+    let message = e.target.elements[0];
+    message.textContent = ''
+    socket.emit('msgFromFront', {sender: username, content: message.value})
+  })
+  
+  document.querySelector('input').addEventListener('focus', e => {
+    socket.emit('typing', null)
+  })
 })
--- server.js
+++ server.js
@@ -1,24 +1,41 @@
+// Express and Socket io importation
 const express = require('express');
 const socketio = require('socket.io');
 const path = require('path');
 
 
+// The main app
 const app = express();
+
 const server = require('http').createServer(app)
-const io = socketio(server);
+const io = socketio(server);//instance of socket io 
+
 
 let port = process.env.PORT || 8000;
 
+// Static middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
+// on connection to the web socket
+
 io.on('connection', socket => {
-  socket.emit('message', 'Hello')
-  socket.broadcast.emit('message', "There is a new client");
+  //anytime there's is a new connection, a socket object
+  // is created for each class with a couple of useful methods
+
+  socket.emit('message', {sender: 'admin', content:'Hello'}) //Welcome message to new comers in the group
+
+  socket.broadcast.emit('message', {sender: 'admin', content: "There is a new client"}) //message to existing clients when a new client joins;
+  
   socket.on('msgFromFront', message => {
     io.emit('message', message)
   })
+
+  socket.on('typing', stuff => {
+    socket.broadcast.emit('showtyping', stuff)
+  })
+
   socket.on('disconnect', stuff => {
-    socket.broadcast.emit('message', 'someone has left the group');
+    socket.broadcast.emit('message', {sender: 'admin', content: 'someone has left the group'});
   })
 })
 server.listen(port, () => console.log(`Server has been started on port ${port}`))
