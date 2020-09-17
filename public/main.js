09dacbcce09f07197eb6b0d332fff18a7d0a83f1
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-09-17 07:20:50 +0100

    got the socket working

--- public/main.js
+++ public/main.js
@@ -1,5 +1,16 @@
 const socket = io();
 
-function addNewMessage(messageText){
-  
+function addNewMessage(message){
+  document.querySelector('.messageContainer').innerHTML+= `<div class='message'>${message}</div>`
 }
+
+socket.on('message', message => addNewMessage(message))
+const $form = document.querySelector('form');
+
+
+
+$form.addEventListener('submit', e => {
+  e.preventDefault();
+  let message = e.target.elements[0].value;
+  socket.emit('msgFromFront', message)
+})
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
+const path = require('path');
+
 
 const app = express();
-const io = socketio(http.createServer(app));
+const server = require('http').createServer(app)
+const io = socketio(server);
 
-let port = process.env.PORT || 2000;
+let port = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
-  console.log('there is a fkal;s');
-  console.log(socket);
+  socket.emit('message', 'Hello')
+  socket.broadcast.emit('message', "There is a new client");
+  socket.on('msgFromFront', message => {
+    io.emit('message', message)
+  })
+  socket.on('disconnect', stuff => {
+    socket.broadcast.emit('message', 'someone has left the group');
+  })
 })
-app.listen(port, () => console.log(`Server has been started on port ${port}`))
+server.listen(port, () => console.log(`Server has been started on port ${port}`))
